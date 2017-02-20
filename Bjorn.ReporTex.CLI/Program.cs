708387ba9162df45a62bd4ab8e4b058a63b4795d
CAndRyan <CAndRyan@users.noreply.github.com>
2017-02-20 18:33:32 -0500

    Testing additional template options

--- Bjorn.ReporTex.CLI/Program.cs
+++ Bjorn.ReporTex.CLI/Program.cs
@@ -11,9 +11,14 @@ namespace Bjorn.ReporTex.CLI {
             ITemplateEngine stencil = new Stencil.Stencil();
             IConverter converter = new Converter();
 
-            IFile compiled = stencil.Compile("Sample_Report", new { Author = "Chris", Title = "A Sample Report" });
-            IBuiltFile built = converter.Convert(compiled, FileFormat.Pdf);
-            converter.Cleanup(built, true);
+            string consoleOutput = String.Empty;
+
+            IFile compiled = stencil.Compile("Sample_Data", null);
+            //IFile compiled = stencil.Compile("Sample_Data", new { Author = "Chris", Title = "A Sample Report" });
+            IBuiltFile built = converter.Convert(compiled, FileFormat.Pdf, out consoleOutput);
+            //converter.Cleanup(built, true);
+
+            Console.WriteLine(String.Format("\n{0}\n", consoleOutput));
 
             Console.WriteLine(String.Format("\nBuilt to: {0}", built.Path));
             Console.WriteLine("\nPress any key to exit...");
--- Bjorn.ReporTex.Stencil/TemplateEngine.cs
+++ Bjorn.ReporTex.Stencil/TemplateEngine.cs
@@ -7,6 +7,9 @@ using RazorEngine;
 using RazorEngine.Templating;
 using RazorEngine.Configuration;
 using RazorEngine.Text;
+using System.Security.Policy;
+using System.Security;
+using System.Web.Razor;
 
 namespace Bjorn.ReporTex.Stencil {
     public interface ITemplateEngine {
@@ -20,12 +23,14 @@ namespace Bjorn.ReporTex.Stencil {
         public string TemplateDirectory { get; private set; } = TEMPLATE_DIRECTORY;
         public string OutputDirectory { get; private set; } = OUTPUT_DIRECTORY;
         private TemplateServiceConfiguration Config;
+        //private IRazorEngineService IsolatedRazorEngine;
 
         public Stencil () {
             Config = new TemplateServiceConfiguration();
             Config.Language = Language.CSharp;
             Config.EncodedStringFactory = new RawStringFactory();
-            
+
+            //IsolatedRazorEngine = IsolatedRazorEngineService.Create(new LanguageEncodingConfigCreator());
             Engine.Razor = RazorEngineService.Create(Config);
         }
 
@@ -45,5 +50,82 @@ namespace Bjorn.ReporTex.Stencil {
             string output = SystemHelper.GetFilePath(OutputDirectory, source.Name, "tex");
             return SystemHelper.WriteFile(output, tex);
         }
+
+        //public static AppDomain SandboxCreator() {
+        //    Evidence ev = new Evidence();
+        //    ev.AddHostEvidence(new Zone(SecurityZone.Internet));
+        //    PermissionSet permSet = SecurityManager.GetStandardSandbox(ev);
+        //    // We have to load ourself with full trust
+        //    StrongName razorEngineAssembly = typeof(RazorEngineService).Assembly.Evidence.GetHostEvidence<StrongName>();
+        //    // We have to load Razor with full trust (so all methods are SecurityCritical)
+        //    // This is because we apply AllowPartiallyTrustedCallers to RazorEngine, because
+        //    // We need the untrusted (transparent) code to be able to inherit TemplateBase.
+        //    // Because in the normal environment/appdomain we run as full trust and the Razor assembly has no security attributes
+        //    // it will be completely SecurityCritical. 
+        //    // This means we have to mark a lot of our members SecurityCritical (which is fine).
+        //    // However in the sandbox domain we have partial trust and because razor has no Security attributes that means the
+        //    // code will be transparent (this is where we get a lot of exceptions, because we now have different security attributes)
+        //    // To work around this we give Razor full trust in the sandbox as well.
+        //    StrongName razorAssembly = typeof(RazorTemplateEngine).Assembly.Evidence.GetHostEvidence<StrongName>();
+        //    AppDomainSetup adSetup = new AppDomainSetup();
+        //    adSetup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+        //    AppDomain newDomain = AppDomain.CreateDomain("Sandbox", null, adSetup, permSet, razorEngineAssembly, razorAssembly);
+        //    return newDomain;
+        //}
     }
+    /// <summary>
+    /// A helper interface to get a custom configuration into a new AppDomain.
+    /// Classes inheriting this interface should be Serializable 
+    /// (and not inherit from MarshalByRefObject).
+    /// </summary>
+    //public interface IConfigCreator {
+    //    /// <summary>
+    //    /// Create a new configuration instance.
+    //    /// This method should be executed in the new AppDomain.
+    //    /// </summary>
+    //    /// <returns></returns>
+    //    ITemplateServiceConfiguration CreateConfiguration();
+    //}
+
+    /// <summary>
+    /// A simple <see cref="IConfigCreator"/> implementation to configure the <see cref="Language"/> and the <see cref="Encoding"/>.
+    /// </summary>
+    //[Serializable]
+    //public class LanguageEncodingConfigCreator : IsolatedRazorEngineService.IConfigCreator {
+    //    private Language language;
+    //    private RazorEngine.Encoding encoding;
+
+    //    /// <summary>
+    //    /// Initializes a new <see cref="LanguageEncodingConfigCreator"/> instance
+    //    /// </summary>
+    //    /// <param name="language"></param>
+    //    /// <param name="encoding"></param>
+    //    public LanguageEncodingConfigCreator(Language language = Language.CSharp, RazorEngine.Encoding encoding = RazorEngine.Encoding.Raw) {
+    //        this.language = language;
+    //        this.encoding = encoding;
+    //    }
+
+    //    /// <summary>
+    //    /// Create the configuration.
+    //    /// </summary>
+    //    /// <returns></returns>
+    //    public ITemplateServiceConfiguration CreateConfiguration() {
+    //        IEncodedStringFactory factory = null;
+    //        switch (encoding) {
+    //            case RazorEngine.Encoding.Html:
+    //                factory = new HtmlEncodedStringFactory();
+    //                break;
+    //            case RazorEngine.Encoding.Raw:
+    //                factory = new RawStringFactory();
+    //                break;
+    //            default:
+    //                throw new NotImplementedException();
+    //        }
+
+    //        return new TemplateServiceConfiguration() {
+    //            Language = language,
+    //            EncodedStringFactory = factory
+    //        };
+    //    }
+    //}
 }
